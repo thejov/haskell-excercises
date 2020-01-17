7d979252b958bf62a1c8cad1b0a35356390f801f
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-01-17 23:10:40 +0200

    Add take'

--- recursion.hs
+++ recursion.hs
@@ -7,3 +7,8 @@ replicate' :: (Int) -> a -> [a]
 replicate' n x
     | n <= 0    = []
     | otherwise = x : replicate' (n-1) x
+
+take' :: (Int) -> [a] -> [a]
+take' n _
+    | n <= 0    = []
+take' n (x:xs)  = x : take' (n-1) xs
