86d53404816bb7d9348bd6a1aeed03d4e5b2e78d
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-01-19 21:10:14 +0200

    Add flip'

--- 05-higher-order-functions.hs
+++ 05-higher-order-functions.hs
@@ -2,3 +2,6 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b  -> c) -> (b -> a -> c)
+flip' f a b = f b a
