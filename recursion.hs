0b440e4af001cdf0e1697759122b22f0cc100811
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-01-19 17:00:05 +0200

    Fix linter warnings from Hlint

--- recursion.hs
+++ recursion.hs
@@ -3,12 +3,12 @@ maximum' [] = error "Cannot take maximum of an empty list"
 maximum' [x] = x
 maximum' (x:xs) = max x (maximum' xs)
 
-replicate' :: (Int) -> a -> [a]
+replicate' :: Int -> a -> [a]
 replicate' n x
     | n <= 0    = []
     | otherwise = x : replicate' (n-1) x
 
-take' :: (Int) -> [a] -> [a]
+take' :: Int -> [a] -> [a]
 take' n _
     | n <= 0    = []
 take' n (x:xs)  = x : take' (n-1) xs
