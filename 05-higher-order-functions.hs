8607d968ca81c7a8f543d477e031a3c7c70eeee1
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-01-20 20:19:40 +0200

    Add quicksort implemented with filter function

--- 05-higher-order-functions.hs
+++ 05-higher-order-functions.hs
@@ -5,3 +5,7 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
 flip' :: (a -> b  -> c) -> (b -> a -> c)
 flip' f a b = f b a
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = quicksort (filter (<= x) xs) ++ [x] ++ quicksort (filter (> x) xs)
