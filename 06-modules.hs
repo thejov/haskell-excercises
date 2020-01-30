edcbdbd18382ce48ad245487c85edafe1aac96cd
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-01-30 21:15:48 +0200

    Add numEachWordInString

--- 06-modules.hs
+++ 06-modules.hs
@@ -2,3 +2,6 @@ import Data.List
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+numEachWordInString :: String -> [(String, Int)]
+numEachWordInString = map (\word -> (head word,length word)) . group . sort . words
