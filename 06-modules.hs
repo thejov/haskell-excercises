279bc314dd1a16f13e3611d39976509d9742f809
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-01-31 20:30:41 +0200

    Add rot
    Kinda like rot13, but you get to decide the offset

--- 06-modules.hs
+++ 06-modules.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Char
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -8,3 +9,6 @@ numEachWordInString = map (\word -> (head word,length word)) . group . sort . wo
 
 isContainedIn :: (Eq a) => [a] -> [a] -> Bool
 isContainedIn xs ys = any (isPrefixOf xs) (tails ys)
+
+rot :: Int -> String -> String
+rot n = map (chr . (+n) . ord)
