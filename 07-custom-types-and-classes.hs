7a1bbba6e40731ccec3afcf546ea3a03144f4b05
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-02-15 20:14:36 +0200

    Add origoCircle and origoRectangle

--- 07-custom-types-and-classes.hs
+++ 07-custom-types-and-classes.hs
@@ -9,3 +9,9 @@ nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) nudgeX nudgeY = Circle (Point (x + nudgeX) (y + nudgeY)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) nudgeX nudgeY
     = Rectangle (Point (x1 + nudgeX) (y1 + nudgeY)) (Point (x2 + nudgeX) (y2 + nudgeY))
+
+origoCircle :: Float -> Shape
+origoCircle r = Circle (Point 0 0) r
+
+origoRectangle :: Float -> Float -> Shape
+origoRectangle height width = Rectangle (Point 0 0) (Point height width)
