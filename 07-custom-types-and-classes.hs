22ff15aff897e662adbb3d2b8671d0bebb03a031
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-02-15 20:08:03 +0200

    Add nudge

--- 07-custom-types-and-classes.hs
+++ 07-custom-types-and-classes.hs
@@ -4,3 +4,8 @@ data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 area :: Shape -> Float
 area (Circle _ r) = pi * r ^ 2
 area (Rectangle (Point x1 y1) (Point x2 y2)) = abs (x2 - x1) * abs (y2 -y1)
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) nudgeX nudgeY = Circle (Point (x + nudgeX) (y + nudgeY)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) nudgeX nudgeY
+    = Rectangle (Point (x1 + nudgeX) (y1 + nudgeY)) (Point (x2 + nudgeX) (y2 + nudgeY))
