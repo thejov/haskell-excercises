89851db81fa0dc19270bc61405e87b7c0245d556
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-03-30 22:00:18 +0300

    Find shortest route

--- 10-functionally-solving-problems/shortest-route.hs
+++ 10-functionally-solving-problems/shortest-route.hs
@@ -1,16 +1,19 @@
 type Section = (Int,Int,Int)
+type Route = [Int]
 
-shortestRoute :: [Section] -> Section
-shortestRoute = foldl shortestRouteInSection (0,0,0)
+shortestRoute :: [Section] -> Route
+shortestRoute = pickShorterRoute . foldl shortestRouteInSection ([],[])
 
-shortestRouteInSection :: Section -> Section -> Section
-shortestRouteInSection (acc_a, acc_b, _) (a,b,c) = (
-    min (acc_a+a) (acc_b+b+c),
-    min (acc_b+b) (acc_a+a+c),
-    0
-  )
+shortestRouteInSection :: (Route, Route) -> Section -> (Route,Route)
+shortestRouteInSection (routeToA, routeToB) (a,b,c) = 
+  (pickShorterRoute (routeToA++[a], routeToB ++ [b] ++ [c]), pickShorterRoute (routeToB++[b], routeToA ++ [a] ++ [c]))
 
-sections =
+pickShorterRoute :: (Route,Route) -> Route
+pickShorterRoute (routeA,routeB)
+  | sum routeA < sum routeB = routeA
+  | otherwise               = routeB
+
+exampleSections =
   [
     (50,10,30),
     (5,90,20),
@@ -18,4 +21,4 @@ sections =
     (10,8,0)
   ]
 
-shortest = shortestRoute sections
+shortest = shortestRoute exampleSections
