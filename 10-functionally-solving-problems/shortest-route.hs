1e16309826057ac7f85304327d7e6d5c81d5e579
Juha Viljanen <juha.viljanen@smartly.io>
2020-04-06 21:07:27 +0300

    Take input from file

--- 10-functionally-solving-problems/shortest-route.hs
+++ 10-functionally-solving-problems/shortest-route.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 type Section = (Int, Int, Int)
 data Label = A | B | C deriving (Show)
 type Route = [(Label, Int)]
@@ -12,7 +14,22 @@ shortestRouteInSection (routeToA, routeToB) (a,b,c) =
 pickShorterRoute :: (Route,Route) -> Route
 pickShorterRoute (routeA,routeB)
   | sum (map snd routeA) < sum (map snd routeB) = routeA
-  | otherwise               = routeB
+  | otherwise = routeB
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+main = do
+  contents <- getContents
+  let threes = groupsOf 3 (map read $ lines contents)
+      sections = map (\[a,b,c] -> (a,b,c)) threes
+      route = shortestRoute sections
+      routeString = concat $ map (show . fst) route
+      routeTime = sum $ map snd route
+  putStrLn $ "The shortest route to take is: " ++ routeString
+  putStrLn $ "Time taken: " ++ show routeTime
 
 exampleSections =
   [
