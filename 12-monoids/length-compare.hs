47933a00463998b8f39c43e652c8d6078d704d02
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-05-13 20:33:19 +0300

    Add one more condition

--- 12-monoids/length-compare.hs
+++ 12-monoids/length-compare.hs
@@ -1,6 +1,10 @@
 import Data.Monoid
 
--- Get Ordering of string lengths or alphabetical Orderding if lengths are equal
+-- Get Ordering of string lengths
+-- or the Ordering based on amount of vowels in the strings if lengths are equal
+-- or alphabetical Orderding if the vocel counts are equal
 lengthCompare :: String -> String -> Ordering
 lengthCompare x y = (length x `compare` length y) `mappend`
+                    (vowels x `compare` vowels y) `mappend`
                     (x `compare` y)
+     where vowels = length . filter (`elem` "aeiou")
