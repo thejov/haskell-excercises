211a6f1d5f8943366facf2632263e08ad55adff5
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-05-13 20:25:09 +0300

    Refactor to take advantage of mappend

--- 12-monoids/length-compare.hs
+++ 12-monoids/length-compare.hs
@@ -1,5 +1,6 @@
+import Data.Monoid
+
 -- Get Ordering of string lengths or alphabetical Orderding if lengths are equal
 lengthCompare :: String -> String -> Ordering
-lengthCompare x y = let a = length x `compare` length y
-                        b = x `compare` y
-                    in if a == EQ then b else a
+lengthCompare x y = (length x `compare` length y) `mappend`
+                    (x `compare` y)
