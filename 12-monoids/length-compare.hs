9978eeb393ab2c1f2f7ec1f999ad273b618ab9cc
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-05-13 20:21:07 +0300

    Non-monoid implementation

---
+++ 12-monoids/length-compare.hs
@@ -0,0 +1,5 @@
+-- Get Ordering of string lengths or alphabetical Orderding if lengths are equal
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y = let a = length x `compare` length y
+                        b = x `compare` y
+                    in if a == EQ then b else a
