1fcf021c1084f9640840fb4c33eb66a56f13510d
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-06-16 19:55:58 +0300

    Initial tightrope walker

---
+++ 13-monads/tightrope-walker.hs
@@ -0,0 +1,10 @@
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Pole
+landLeft n (left, right) = (left + n, right)
+
+landRight :: Birds -> Pole -> Pole
+landRight n (left, right) = (left, right + n)
+
+x -: f = f x
