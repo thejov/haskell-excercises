6a7ddd6cb6219e9f305ab60d8ea46fcd6cbc2e4c
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-06-17 20:05:58 +0300

    Build same routine with do notation

--- 13-monads/tightrope-walker.hs
+++ 13-monads/tightrope-walker.hs
@@ -17,4 +17,11 @@ banana _ = Nothing
 routine :: Maybe Pole
 routine = landLeft 2 (0,0) >>= landRight 2 >>= landLeft 1
 
+routine' :: Maybe Pole
+routine' = do
+  start <- return (0,0)
+  first <- landLeft 2 start
+  second <- landRight 2 first
+  landLeft 1 second
+
 x -: f = f x
