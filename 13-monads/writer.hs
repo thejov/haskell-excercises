7bb84f995b7373117e444aebbd10c0270b6e32c9
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-07-02 20:48:43 +0300

    addDrinkToFoodOrder that's chainable with applyLog

--- 13-monads/writer.hs
+++ 13-monads/writer.hs
@@ -1,5 +1,15 @@
+import Data.Monoid
+
 isBigGang :: Int -> (Bool, String)
 isBigGang x = (x > 9, "Compared gang size to 9.")
 
 applyLog :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
 applyLog (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+
+type Food = String
+type Price = Sum Int
+
+addDrinkToFoodOrder :: Food -> (Food, Price)
+addDrinkToFoodOrder "beans" = ("milk", Sum 25)
+addDrinkToFoodOrder "jerky" = ("whiskey", Sum 99)
+addDrinkToFoodOrder _ = ("beer", Sum 30)
