43ad114fc94fc2f73e87b697df887aa7a17c802a
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-10-19 13:28:52 +0300

    Add gcdReverse using difference lists

--- 14-more-monads/gcdReverse.hs
+++ 14-more-monads/gcdReverse.hs
@@ -1,3 +1,5 @@
+import Control.Monad.Writer
+
 newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
 
 toDiffList :: [a] -> DiffList a
@@ -11,3 +13,13 @@ instance Semigroup (DiffList a) where
 
 instance Monoid (DiffList a) where
   mempty = DiffList (\xs -> [] ++ xs)
+
+gcdReverse :: Int -> Int -> Writer (DiffList String) Int
+gcdReverse a b
+  | b == 0 = do
+    tell (toDiffList ["Finished with " ++ show a])
+    return a
+  | otherwise = do
+    result <- gcdReverse b (a `mod` b)
+    tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+    return result
