25d79e178e0a6600d2f557179ebc4245959f4e40
Juha Viljanen <juha.o.viljanen@gmail.com>
2020-10-20 12:41:26 +0300

    Compare performance of diff lists

--- 14-more-monads/gcdReverse.hs
+++ 14-more-monads/gcdReverse.hs
@@ -23,3 +23,17 @@ gcdReverse a b
     result <- gcdReverse b (a `mod` b)
     tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
     return result
+
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+  tell (toDiffList [""])
+finalCountDown x = do
+  finalCountDown (x-1)
+  tell (toDiffList [show x])
+
+finalCountDown' :: Int -> Writer [String] ()
+finalCountDown' 0 = do
+  tell ["0"]
+finalCountDown' x = do
+  finalCountDown' (x-1)
+  tell [show x]
